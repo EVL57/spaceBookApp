688ee99bb40cc6f1d894d5db03fe99d2a0e072c0
EVL57 <99217323+EVL57@users.noreply.github.com>
2022-02-20 19:35:14 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-class HelloWorldApp extends Component {
-  render() {
+import LoginScreen from './components/login';
+import FeedScreen from './components/feed';
+import SignupScreen from './components/signup';
+
+const Tab = createBottomTabNavigator();
+
+class App extends Component {
+
+  render(){
     return (
-      <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Login" component={LoginScreen} />
+          <Tab.Screen name="Feed" component={FeedScreen} />
+          <Tab.Screen name="Signup" component={SignupScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     );
   }
+  
 }
 
-export default HelloWorldApp;
+
+export default App;
